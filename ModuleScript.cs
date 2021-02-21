8ef885fd3401df15c51380cedb8ddafb2e690ee3
Emik03 <emik.lakunishok@gmail.com>
2021-02-21 23:08:22 +0100

    Removed redundant code

--- ModuleScript.cs
+++ ModuleScript.cs
@@ -11,27 +11,27 @@ namespace EmikBaseModules
         /// <summary>
         /// Instance of a regular module. Used to get the name of the module.
         /// </summary>
-        internal virtual KMBombModule KMBombModule { get; set; }
+        internal virtual KMBombModule KMBombModule { get; private set; }
 
         /// <summary>
         /// Instance of a boss module, is used to set the ignore list.
         /// </summary>
-        internal virtual KMBossModule KMBossModule { get; set; }
+        internal virtual KMBossModule KMBossModule { get; private set; }
 
         /// <summary>
         /// Accesses the colorblind mod in-game, is used to set IsColorblind.
         /// </summary>
-        internal virtual KMColorblindMode KMColorblindMode { get; set; }
+        internal virtual KMColorblindMode KMColorblindMode { get; private set; }
 
         /// <summary>
         /// Instance of a needy module. Used to get the name of the module.
         /// </summary>
-        internal virtual KMNeedyModule KMNeedyModule { get; set; }
+        internal virtual KMNeedyModule KMNeedyModule { get; private set; }
 
         /// <summary>
         /// Called whenever the timer tick changes. Requires KMBombInfo to access the time left.
         /// </summary>
-        internal virtual Tuple<Action, KMBombInfo> OnTimerTick { get; set; }
+        internal virtual Tuple<Action, KMBombInfo> OnTimerTick { get; private set; }
 
         /// <summary>
         /// The module's display name. This is used for logging.
@@ -52,14 +52,25 @@ namespace EmikBaseModules
         /// Are the lights in-game on? (Has the bomb started the timer?)
         /// </summary>
         internal bool IsActivate { get; private set; }
+
         /// <summary>
         /// Is Colorblind mode enabled? (Using KMColorblindMode)
         /// </summary>
-        internal bool IsColorblind { get; private set; }
+        internal bool IsColorblind
+        { 
+            get 
+            {
+                if (KMColorblindMode == null)
+                    throw new NotImplementedException("IsColorblind expects KMColorblindMode to not be null.");
+                return KMColorblindMode.ColorblindModeActive;
+            } 
+        }
+
         /// <summary>
         /// Is the module played in the editor?
         /// </summary>
-        internal bool IsEditor { get; private set; }
+        internal bool IsEditor { get { return Application.isEditor; } }
+
         /// <summary>
         /// Has the module been solved?
         /// </summary>
@@ -69,10 +80,12 @@ namespace EmikBaseModules
         /// Is TwitchPlays active?
         /// </summary>
         internal bool TwitchPlaysActive;
+
         /// <summary>
         /// Is Time Mode active?
         /// </summary>
         internal bool TimeModeActive;
+
         /// <summary>
         /// Is Zen Mode active?
         /// </summary>
@@ -82,6 +95,7 @@ namespace EmikBaseModules
         /// The last instantiation of the module. Use moduleId == moduleIdCounter when only 1 module needs to do something.
         /// </summary>
         internal static int ModuleIdCounter { get; private set; }
+
         /// <summary>
         /// The moduleId, for logging.
         /// </summary>
@@ -110,7 +124,15 @@ namespace EmikBaseModules
         /// <summary>
         /// Contains modules that should be ignored, typically for boss modules.
         /// </summary>
-        internal string[] IgnoreList { get; set; }
+        internal string[] IgnoreList 
+        {
+            get
+            {
+                if (KMBossModule == null)
+                    throw new NotImplementedException("IgnoreList expects KMBossModule to not be null.");
+                return KMBossModule.GetIgnoredModules(KMBombModule, IgnoreList);
+            }
+        }
 
         /// <summary>
         /// Event initializer.
@@ -120,22 +142,9 @@ namespace EmikBaseModules
             // This gives a unique value of each instantiation, since moduleIdCounter is static.
             ModuleId = ++ModuleIdCounter;
 
-            // This determines if it's running in-game or the editor.
-            IsEditor = Application.isEditor;
-
-            // Sets ignore list.
-            if (KMBossModule != null)
-                IgnoreList = KMBossModule.GetIgnoredModules(KMBombModule, IgnoreList);
-            else if (IgnoreList != null)
-                PanicIfParentNull("KMBossModule", IgnoreList);
-
-            // Sets colorblind.
-            if (KMColorblindMode != null)
-                IsColorblind = KMColorblindMode.ColorblindModeActive;
-
             // Makes sure OnTimerTick is paired with KMBombInfo.
-            if (OnTimerTick.Item2 != null)
-                PanicIfNull("OnTimerTick.Item2", OnTimerTick.Item2);
+            if (OnTimerTick.Item2 == null)
+                throw new NotImplementedException("OnTimerTick has a null KMBombInfo. An instance of KMBombInfo is required to access time remaining.");
         }
 
         /// <summary>
@@ -144,24 +153,7 @@ namespace EmikBaseModules
         private void Update()
         {
             // Updates the amount of time left within the TimeLeft property.
-            if (OnTimerTick.Item2 != null)
-                TimeLeft = (int)OnTimerTick.Item2.GetTime();
-        }
-
-        /// <summary>
-        /// Throws an error if the event handler is null but any of its events are not null, since this is considered a mistake.
-        /// </summary>
-        /// <param name="parent">The parent object's name, since reflection cannot be used to obtain an unknown property.</param>
-        /// <param name="objs">The list of events to do null validation on.</param>
-        private void PanicIfParentNull(string parent, params object[] objs)
-        {
-            const string ErrorMessage = @"{0} is not null despite the parent object {1} being null. Be sure to check that you assigned your properties correctly!";
-
-            for (int i = 0; i < objs.Length; i++)
-            {
-                if (objs[i] != null)
-                    this.Log(ErrorMessage.Format(objs[i], parent), LogType.Error);
-            }
+            TimeLeft = (int)OnTimerTick.Item2.GetTime();
         }
 
         /// <summary>
